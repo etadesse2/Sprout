99eef45a12c8c26de2c1b1c30a0e3a8124b049b7
sarahyosef <sarahyosefux@gmail.com>
2024-03-07 16:32:04 -0500

    removed parts of registration screen

--- sprouts/lib/registration_screen.dart
+++ sprouts/lib/registration_screen.dart
@@ -7,14 +7,13 @@ class RegistrationScreen extends StatelessWidget {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  RegistrationScreen({super.key});
+  RegistrationScreen({Key? key}) : super(key: key);
 
   Future<void> _register(BuildContext context) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString('username', _usernameController.text);
     await prefs.setString('password', _passwordController.text);
     Navigator.pushReplacement(
-      // ignore: use_build_context_synchronously
       context,
       MaterialPageRoute(builder: (context) => HomeScreen()),
     );
@@ -23,7 +22,6 @@ class RegistrationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.only(left: 60, right: 60),
@@ -170,25 +168,6 @@ class RegistrationScreen extends StatelessWidget {
             ),
           ),
         ),
-=======
-      appBar: AppBar(title: const Text('Register')),
-      body: Column(
-        children: <Widget>[
-          TextField(
-            controller: _usernameController,
-            decoration: const InputDecoration(labelText: 'Username'),
-          ),
-          TextField(
-            controller: _passwordController,
-            decoration: const InputDecoration(labelText: 'Password'),
-            obscureText: true,
-          ),
-          ElevatedButton(
-            onPressed: () => _register(context),
-            child: const Text('Register'),
-          ),
-        ],
->>>>>>> 977ee931712002b05d03daf542a5618c3d2550c8
       ),
     );
   }
