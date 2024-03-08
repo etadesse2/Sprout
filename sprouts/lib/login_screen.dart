3ef82dbc25d59c80928a08e612623839d0ea2e11
sarahyosef <sarahyosefux@gmail.com>
2024-03-08 01:14:04 -0500

    login screen validators added

--- sprouts/lib/login_screen.dart
+++ sprouts/lib/login_screen.dart
@@ -6,7 +6,7 @@ import 'registration_screen.dart'; // Import RegistrationScreen
 class LoginScreen extends StatelessWidget {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   LoginScreen({Key? key}) : super(key: key);
 
   Future<void> _login(BuildContext context) async {
@@ -32,117 +32,138 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 60, right: 60),
-            child: Center(
-              child: Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(top: 230),
-                    child: Image(
-                      image: AssetImage('assets/images/leaf.png'),
-                      height: 45,
-                      width: 30,
-                    ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 60, right: 60),
+          child: Center(
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 230),
+                  child: Image(
+                    image: AssetImage('assets/images/leaf.png'),
+                    height: 45,
+                    width: 30,
                   ),
-                  const Padding(
-                    padding: EdgeInsets.only(bottom: 22.0),
-                    child: Text(
-                      'SPROUT',
-                      style: TextStyle(
-                          letterSpacing: 10,
-                          fontSize: 40,
-                          color: Color.fromARGB(255, 28, 67, 30)),
-                    ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 22.0),
+                  child: Text(
+                    'SPROUT',
+                    style: TextStyle(
+                        letterSpacing: 10,
+                        fontSize: 40,
+                        color: Color.fromARGB(255, 28, 67, 30)),
                   ),
-                  TextField(
-                    controller: _usernameController,
-                    decoration: InputDecoration(
-                      border: const OutlineInputBorder(),
-                      hintText: 'Username',
-                      contentPadding: const EdgeInsets.only(left: 30),
-                      hintStyle: const TextStyle(
-                          fontSize: 14, color: Color(0xFFB8B8B8)),
-                      filled: true,
-                      fillColor: const Color(0xFFF2F2F2),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(40),
-                        borderSide: BorderSide.none,
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide:
-                              const BorderSide(color: Color(0xFF134800))),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  TextField(
-                    controller: _passwordController,
-                    decoration: InputDecoration(
-                      border: const OutlineInputBorder(),
-                      hintText: 'Password',
-                      contentPadding: const EdgeInsets.only(left: 30),
-                      hintStyle: const TextStyle(
-                          fontSize: 14, color: Color(0xFFB8B8B8)),
-                      filled: true,
-                      fillColor: const Color(0xFFF2F2F2),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(40),
-                        borderSide: BorderSide.none,
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide:
-                              const BorderSide(color: Color(0xFF134800))),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 180,
-                    height: 65,
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 20.0),
-                      child: ElevatedButton(
-                        onPressed: () => _login(context),
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xFF134800)),
-                        child: const Text('Login',
-                            style: TextStyle(color: Colors.white)),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8.0),
-                    child: SizedBox(
-                      width: 180,
-                      height: 50,
-                      child: FloatingActionButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => RegistrationScreen()));
-                        },
-                        backgroundColor: Color.fromARGB(255, 255, 255, 255),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30)),
-                        elevation: 0,
-                        splashColor: Colors.white,
-                        child: const Text(
-                          'Register',
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 128, 128, 128)),
+                ),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: _usernameController,
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          hintText: 'Username',
+                          contentPadding: const EdgeInsets.only(left: 30),
+                          hintStyle: const TextStyle(
+                              fontSize: 14, color: Color(0xFFB8B8B8)),
+                          filled: true,
+                          fillColor: const Color(0xFFF2F2F2),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(40),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide:
+                                  const BorderSide(color: Color(0xFF134800))),
                         ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter username';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      TextFormField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          hintText: 'Password',
+                          contentPadding: const EdgeInsets.only(left: 30),
+                          hintStyle: const TextStyle(
+                              fontSize: 14, color: Color(0xFFB8B8B8)),
+                          filled: true,
+                          fillColor: const Color(0xFFF2F2F2),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(40),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide:
+                                  const BorderSide(color: Color(0xFF134800))),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter password';
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 180,
+                  height: 65,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _login(context);
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF134800)),
+                      child: const Text('Login',
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: SizedBox(
+                    width: 180,
+                    height: 50,
+                    child: FloatingActionButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => RegistrationScreen()));
+                      },
+                      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30)),
+                      elevation: 0,
+                      splashColor: Colors.white,
+                      child: const Text(
+                        'Register',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 128, 128, 128)),
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
